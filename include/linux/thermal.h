789025c344be7aac09a25037c7ee002755425505
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2024-02-12 14:00:06 +0100

    thermal: core: Store zone trips table in struct thermal_zone_device
    The current code expects thermal zone creators to pass a pointer to a
    writable trips table to thermal_zone_device_register_with_trips() and
    that trips table is then used by the thermal core going forward.
    
    Consequently, the callers of thermal_zone_device_register_with_trips()
    are required to hold on to the trips table passed to it until the given
    thermal zone is unregistered, at which point the trips table can be
    freed, but at the same time they are not expected to access that table
    directly.  This is both error prone and confusing.
    
    To address it, turn the trips table pointer in struct thermal_zone_device
    into a flex array (counted by its num_trips field), allocate it during
    thermal zone device allocation and copy the contents of the trips table
    supplied by the zone creator (which can be const now) into it, which
    will allow the callers of thermal_zone_device_register_with_trips() to
    drop their trip tables right after the zone registration.
    
    This requires the imx thermal driver to be adjusted to store the new
    temperature in its internal trips table in imx_set_trip_temp(), because
    it will be separate from the core's trips table now and it has to be
    explicitly kept in sync with the latter.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -137,7 +137,6 @@ struct thermal_cooling_device {
  * @trip_hyst_attrs:	attributes for trip points for sysfs: trip hysteresis
  * @mode:		current mode of this thermal zone
  * @devdata:	private pointer for device private data
- * @trips:	an array of struct thermal_trip
  * @num_trips:	number of trip points the thermal zone supports
  * @passive_delay_jiffies: number of jiffies to wait between polls when
  *			performing passive cooling.
@@ -167,6 +166,7 @@ struct thermal_cooling_device {
  * @poll_queue:	delayed work for polling
  * @notify_event: Last notification event
  * @suspended: thermal zone suspend indicator
+ * @trips:	array of struct thermal_trip objects
  */
 struct thermal_zone_device {
 	int id;
@@ -179,7 +179,6 @@ struct thermal_zone_device {
 	struct thermal_attr *trip_hyst_attrs;
 	enum thermal_device_mode mode;
 	void *devdata;
-	struct thermal_trip *trips;
 	int num_trips;
 	unsigned long passive_delay_jiffies;
 	unsigned long polling_delay_jiffies;
@@ -200,10 +199,11 @@ struct thermal_zone_device {
 	struct list_head node;
 	struct delayed_work poll_queue;
 	enum thermal_notify_event notify_event;
+	bool suspended;
 #ifdef CONFIG_THERMAL_DEBUGFS
 	struct thermal_debugfs *debugfs;
 #endif
-	bool suspended;
+	struct thermal_trip trips[] __counted_by(num_trips);
 };
 
 /**
@@ -322,7 +322,7 @@ int thermal_zone_get_crit_temp(struct thermal_zone_device *tz, int *temp);
 #ifdef CONFIG_THERMAL
 struct thermal_zone_device *thermal_zone_device_register_with_trips(
 					const char *type,
-					struct thermal_trip *trips,
+					const struct thermal_trip *trips,
 					int num_trips, void *devdata,
 					struct thermal_zone_device_ops *ops,
 					const struct thermal_zone_params *tzp,
@@ -381,7 +381,7 @@ void thermal_zone_device_critical(struct thermal_zone_device *tz);
 #else
 static inline struct thermal_zone_device *thermal_zone_device_register_with_trips(
 					const char *type,
-					struct thermal_trip *trips,
+					const struct thermal_trip *trips,
 					int num_trips, void *devdata,
 					struct thermal_zone_device_ops *ops,
 					const struct thermal_zone_params *tzp,
